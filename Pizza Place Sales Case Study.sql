371e11ec5879b326ab58288af42fb6f9433677cd
Donald Ebube <donaldebube@yahoo.com>
2022-09-25 02:12:13 +0100

    New Commit

--- Pizza Place Sales Case Study.sql
+++ Pizza Place Sales Case Study.sql
@@ -26,6 +26,7 @@ SELECT COUNT(DISTINCT pizza_id) AS [Total Number of Pizza ID]
 FROM OrderDetails
 
 --List of pizza ids that are ordered the most with the sum of quantity ordered for the year
+--Most ordered Pizzas
 SELECT DISTINCT pizza_id, SUM(quantity) AS total_quantity
 FROM OrderDetails
 GROUP BY pizza_id
@@ -39,7 +40,7 @@ GROUP BY  pizza_id, [date]
 ORDER BY quantity DESC
 
 -- list of pizz_id that customers do not order
-SELECT DISTINCT OD.pizza_id, P.pizza_id
+SELECT DISTINCT OD.pizza_id , P.pizza_id AS [Not Ordered By Customers]
 FROM OrderDetails OD
 FULL OUTER JOIN Pizzas P
     ON OD.pizza_id = P.pizza_id
