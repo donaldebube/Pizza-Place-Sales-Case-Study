9da0e05db83cb3e84b658c6be324a85081dc8119
Donald Ebube <donaldebube@yahoo.com>
2022-09-25 22:39:55 +0100

    New Commit

--- Pizza Place Sales Case Study.sql
+++ Pizza Place Sales Case Study.sql
@@ -25,7 +25,7 @@ FROM OrderDetails
 SELECT COUNT(DISTINCT pizza_id) AS [Total Number of Pizza Sold]
 FROM OrderDetails
 
---List of pizza ids that are ordered the most with the sum of quantity ordered for the year
+--List of pizzas that are ordered the most with the sum of quantity ordered for the year
 --Most ordered Pizzas
 SELECT DISTINCT pizza_id, SUM(quantity) AS total_quantity
 FROM OrderDetails
@@ -40,14 +40,19 @@ INNER JOIN Orders O
 GROUP BY CAST([date] AS DATE)
 ORDER BY [quantity] DESC
 
---Pizza with the highest 
-
--- list of pizz_id that customers do not order
+-- List of pizzas that customers do not order
 SELECT DISTINCT OD.pizza_id , P.pizza_id AS [Not Ordered By Customers]
 FROM OrderDetails OD
 FULL OUTER JOIN Pizzas P
     ON OD.pizza_id = P.pizza_id
 WHERE OD.pizza_id IS NULL
+
+-- List of pizzas that customers do order
+SELECT DISTINCT OD.pizza_id , P.pizza_id AS [Ordered]
+FROM OrderDetails OD
+FULL OUTER JOIN Pizzas P
+    ON OD.pizza_id = P.pizza_id
+WHERE OD.pizza_id IS NOT NULL
 
 --Top most expensive Pizza Types
 SELECT P.pizza_type_id, P.price,P.[size], PT.name, PT.category, SUM(OD.quantity) AS total_quantity
